868978931d29fc67457cc8757fbf678ea04383b8
afzaadkhan <126318878+afzaadkhan@users.noreply.github.com>
2024-05-25 16:21:11 +0500

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -23,7 +23,7 @@ const Footer = () => {
       </div>
       <div className="container mx-auto text-center text-xs">
         <p>&copy; 2024 MemeCoin. All rights reserved.</p>
-        <p className="mt-2">Token Contract Address: <span className="font-bold select-all ">0x1234567890abcdef1234567890abcdef12345678</span></p>
+        <p className="mt-2"><span className="font-bold select-all "></span></p>
         <p className="mt-2">Contact: <a href="mailto:info@memecoin.com" className="underline">info@bingus.com</a></p>
       </div>
     </footer>
